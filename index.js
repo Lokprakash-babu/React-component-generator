34cfb7a87cd609c93514809eaf402cac693dc494
Lokprakash-babu <lokprakash100@gmail.com>
2021-06-27 11:27:46 +0530

    included pwd

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const chalk = require("chalk");
 const log = console.log;
 const npath = require("path");
+const { exec } = require("child_process");
 
 yargs.version("1.0.0");
 
@@ -23,7 +24,9 @@ function getName(argV){
 function checkPath(path){
     const pathCheck = path.split("/");
     pathCheck.pop();
+    // console.log("pathCheck", pathCheck);
     const newPath = pathCheck.join("/");
+    // console.log("new path", newPath);
     return new Promise((res, _)=>{
         fs.access(newPath, function(error){
              error?res(false):res(true);
@@ -36,6 +39,7 @@ function generateDir(path){
     path = path.split("/");
     path.pop();
     path = path.join("/");
+    // console.log("path in mkdir", path);
     return new Promise((res, rej)=>{
         fs.mkdir(path, {recursive: true}, (err)=>{
             err?rej(err):res("success in creating the directory");
@@ -67,48 +71,67 @@ yargs.parse();
 
 
 async function createComponent(argV){
-    log(npath.dirname(require.main.filename));   
-    const homePath =npath.dirname(require.main.filename);
-    let name = getName(argV);
-    name = name.slice(0,1).toUpperCase()+name.slice(1);
+    exec("pwd", async(err, stdOut, stdErr)=>{
+        if(err){
+            console.log("error", err);
+            return;
+        }
+        if(stdErr){
+            console.log("stdErr", stdErr);
+            return;
+        }
+        stdOut = stdOut.split("\\").slice(2).join("/");
+        // console.log("stdOut", npath.resolve(stdOut));
+        const homePath =npath.resolve(stdOut);
+        let name = getName(argV);
+        name = name.slice(0,1).toUpperCase()+name.slice(1);
 
-// function component file content
-    let fc = `
-import React from 'react';
+    // function component file content
+        let fc = `
+    import React from 'react';
 
-const ${name} = ()=>{
-    return(
-        <div>${name} works</div>
-    );
-}
-
-export default ${name};
-`;
-
-//if class component to be generated argV.c = true
-    if(argV.c){
-        fc=`
-import React, {Component} from 'react';
-
-class ${name} extends Component{
-    render() {
-        return <h1>${name} works</h1>;
-    }
-}
-
-export default ${name};
-`;
+    const ${name} = ()=>{
+        return(
+            <div>${name} works</div>
+        );
     }
 
-    //checking path of the directory 
-    const pathCheck = await checkPath(homePath+"/src/"+argV.p); 
-    if(pathCheck){
-        genFile(homePath+"/src/"+argV.p+".jsx", fc, name, argV);
+    export default ${name};
+    `;
+
+    //if class component to be generated argV.c = true
+        if(argV.c){
+            fc=`
+    import React, {Component} from 'react';
+
+    class ${name} extends Component{
+        render() {
+            return <h1>${name} works</h1>;
+        }
     }
-    else{
-        await generateDir(homePath+"/src/"+argV.p+".jsx");
-        genFile(homePath+"/src/"+argV.p+".jsx", fc, name, argV);
-    }
+
+    export default ${name};
+    `;
+        }
+
+        //checking path of the directory 
+        // console.log("path to check", homePath+"/src/"+argV.p);
+        const pathCheck = await checkPath(homePath+"/src/"+argV.p); 
+        if(pathCheck){
+            genFile(homePath+"/src/"+argV.p+".jsx", fc, name, argV);
+        }
+        else{
+            try{
+
+                await generateDir(homePath+"/src/"+argV.p+".jsx");
+                genFile(homePath+"/src/"+argV.p+".jsx", fc, name, argV);
+            }
+            catch(err){
+                console.error("error in the else block", err);
+            }
+        }
+    })
+    
 }
 
 
